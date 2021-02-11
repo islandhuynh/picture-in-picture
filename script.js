714a7ff78ab54b7e6dd271bb96473f9fa1e9352c
unknown <islandhuynh@gmail.com>
2021-02-11 16:55:06 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element, then play
+const selectMediaStream = async () => {
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('Whoops, error here: ', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+
+    // Reset Button
+    button.distabled = false;
+});
+
+// On Load
+selectMediaStream();
